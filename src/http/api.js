319d81fd625cca8ba8d3151c812a8ba7a098efb2
binrege <75228158@qq.com>
2020-07-12 23:52:11 +0800

    新增接口

--- src/http/api.js
+++ src/http/api.js
@@ -11,5 +11,36 @@ export default {
     // 获取所有广告
     getAllAd() {
         return service.get('/ads/getAllAd')
-    }
+    },
+
+    //jfy
+    //查询所有的套餐/精品套餐
+    getfindAllCombo(){
+        return service.get('/combo/findAllCombo')
+    },
+    //查询热门课程
+    findHotCourse(){
+        return service.get('/course/findHotCourse')
+    },
+    //获取所有的热门资讯
+    getAllHotInfo(){
+        return service.get('/infos/getAllHotInfo')
+    },
+    //根据资讯类别获取对应的资讯
+    getInfoByInfoType(){
+        return service.get('/infos/getInfoByInfoType')
+    },
+
+    //根据课程id课程目录
+    findCatalogById(){
+        return service.get('/course/findCatalogById')
+    },
+
+    //查询所有老师/teacher/findAll  
+      getfindAllCombo(){
+        return service.get('/teacher/findAll')
+    },
+
+
+    //end jfy
 }
